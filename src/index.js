c14879ee05b5dc527253a9b5d6d901300b946c20
Gregor Zurowski <gregor@zurowski.net>
2020-12-29 21:44:45 +0100

    Section 2.19: Referencing JS variables in JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 const App = () => {
-    return <div>Hi there!</div>;
+    const labelText = 'Enter name:'
+
+    return (
+        <div>
+        <label className="label" for="name">
+            {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {getButtonText()}
+        </button>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
