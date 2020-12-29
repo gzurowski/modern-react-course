62d5ed11202b2e4a0fbe56dcf0799817504f4616
Gregor Zurowski <gregor@zurowski.net>
2020-12-30 00:45:38 +0100

    Section 2.41: Component reuse

--- src/index.js
+++ src/index.js
@@ -8,22 +8,35 @@ const App = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
                 <CommentDetail
                     author="Sam"
                     timeAgo="Today at 6:00pm"
                     content="content1"
                     avatar={faker.image.animals()} />
             </ApprovalCard>
-            <CommentDetail
-                author="Gregor"
-                timeAgo="Today at 2:00am"
-                content="content2"
-                avatar={faker.image.animals()} />
-            <CommentDetail
-                author="Stephen"
-                timeAgo="Yesterday at 9:00am"
-                content="content3"
-                avatar={faker.image.animals()} />
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Gregor"
+                    timeAgo="Today at 2:00am"
+                    content="content2"
+                    avatar={faker.image.animals()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Stephen"
+                    timeAgo="Yesterday at 9:00am"
+                    content="content3"
+                    avatar={faker.image.animals()} />
+            </ApprovalCard>
         </div>
     );
 };
