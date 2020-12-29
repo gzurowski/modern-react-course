57b4c9417ee194eda9b585ba716bb3a4d279b914
Gregor Zurowski <gregor@zurowski.net>
2020-12-29 21:52:03 +0100

    Section 2.21: Fix HTML for warning

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-    return 'Click on me!';
-}
-
 const App = () => {
-    const labelText = 'Enter name:'
+    const buttonText = { text: 'Click me'};
+    const labelText = 'Enter name:';
 
     return (
         <div>
-        <label className="label" for="name">
+        <label className="label" htmlFor="name">
             {labelText}
         </label>
         <input id="name" type="text" />
         <button style={{backgroundColor: 'blue', color: 'white'}}>
-            {getButtonText()}
+            {buttonText.text}
         </button>
         </div>
     );
